7fc6dab2770f246249f55672e04b9099a8771bd8
Oscar Aquino <111005752+0scar00@users.noreply.github.com>
2026-05-06 19:11:53 -0400

    Deployment files

--- website/labtrack/includes/database-connection.php
+++ website/labtrack/includes/database-connection.php
@@ -1,19 +1,18 @@
 <?php
 
 $type     = 'mysql';
-$server   = '127.0.0.1';
+$server   = 'localhost';
 $db       = 'oscaraqu_labtrack';
-$port     = '3307';
 $charset  = 'utf8mb4';
 
-$username = 'root';   // default for XAMPP
-$password = '';       // default for XAMPP
+$username = 'oscaraqu_labtrack_user';
+$password = 'CHANGE_ME';
 
 $options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
-$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset";
+$dsn = "$type:host=$server;dbname=$db;charset=$charset";
 $pdo = new PDO($dsn, $username, $password, $options);
